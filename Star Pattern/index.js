5d6dea1478b99fb679155934dd9050445c37d3fc
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-07-24 11:36:08 +0530

    add

---
+++ Star Pattern/index.js
@@ -0,0 +1,31 @@
+//  Pattern Printing Questions (Very Common in Interviews):
+
+// Q.	 Square Star Pattern
+// let num = 10;
+// for(let i = 1; i <= num; i++){
+//   let row = ''
+//   for(let j = 1; j <= num; j++){
+//     row += '*  ';
+//   }
+//   console.log(row)
+// }
+
+// Q.  Right-Angled Triangle Pattern
+// let n = 10; 
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += "*  ";
+//   }
+//   console.log(row);
+// }
+
+//Q.  Inverted Right-Angled Triangle
+// let num = 10;
+// for(let i = num; i >= 1; i--){
+//   row = ' '
+//   for(let j = 1; j <= i; j++){
+//     row += '*  '
+//   }
+//   console.log(row)
+// }
