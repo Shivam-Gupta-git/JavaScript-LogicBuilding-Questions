1cd13d09ffba9e1fd8ee57e9648330a2bf6962e2
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-07-26 09:20:01 +0530

    Hosting Exaples

---
+++ Hosting/index.js
@@ -0,0 +1,22 @@
+// var declarations are hoisted, but only the declaration (var name), not the assignment
+// console.log(studentName1)
+// var studentName1 = 'Mohan'
+
+// let and const are also hoisted but not initialized.
+// console.log(studentName2)
+// let studentName2 = 'Mohan'
+
+// console.log(studentName3)
+// const studentName3 = 'Mohan'
+
+
+// studentName4()
+// function studentName4(){
+//   console.log('shivam kumar')
+// }
+
+// function expression and class expression can't hosted
+// studentName5()
+// const studentName5 = () => {
+//   console.log('shivam kumar')
+// }
