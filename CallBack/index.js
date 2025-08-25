2e5d944e25a27ce54090fd1116dc309654602290
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-08-25 11:31:33 +0530

    Callback

---
+++ CallBack/index.js
@@ -0,0 +1,32 @@
+// Callback = callback is a function that pass as a arrgument to another function. that is executed after some operation has been completed.
+
+//-> Exapmle: 
+let studentName = (name, callback) => {
+  console.log("student Name is "+ name)
+  callback();
+}
+
+let message = ()=> {
+  console.log("Thank you for entering your name !")
+}
+studentName('Rohan', message)
+
+// -> Example:
+// let sum = (a, b, callback) => {
+//   let result = a + b;
+//   callback(result)
+// } 
+// let message = (result) => {
+//  console.log('sum of your digit is: '+ result);
+//  setTimeout(()=> {
+//   console.log('your task is completed')
+//  }, 2000)
+// }
+// sum(3, 5, message)
+
+// -> Example:
+// let numbers = [2, 4, 6, 1];
+// numbers.forEach((num) => {
+//   console.log(num)
+// })
+
