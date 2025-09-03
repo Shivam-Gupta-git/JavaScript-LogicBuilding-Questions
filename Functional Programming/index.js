6cbd0c88878aeb0a79e522995f9a6e7f238647eb
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-09-03 12:15:13 +0530

    Functional Programming

---
+++ Functional Programming/index.js
@@ -0,0 +1,39 @@
+// -> Functional Programming ........................
+
+// -> Impretavie Programming => work will be do step by step.
+// let numbers = [1, 4, 6, 8, 9]
+// let doubleOfNum = [];
+// for(let i = 0; i < numbers.length; i++){
+//   doubleOfNum.push(numbers[i] * 2)
+// }
+// console.log(doubleOfNum);
+
+// -> Declarative Programming = work will not be do step by stem . only told what do you want.
+// let numbers = [2, 4, 3, 5, 7, 1]
+// let doubleNum = numbers.map(num => num * 2)
+// console.log(doubleNum)
+
+// -> Pure Function = A function that always gives the same output for the same input.
+// let sum = (a, b)=>{
+//   return a + b;
+// }
+// console.log(sum(4, 6))
+
+// -> Immutability = Data should not be changed (mutated). Instead, create new data structures.
+// let numbers = [1, 3, 5]
+// let newNumbers = [...numbers, 7]
+// console.log(newNumbers)
+
+// -> Higher-Order Functions = 	Functions that take other functions as arguments or return them.
+// let numbers = [2, 5, 3, 6, 8]
+// let powerOfNum = numbers.map(num => Math.pow(num, 2))
+// console.log(powerOfNum)
+
+// -> Function Composition = •	Combining small functions to build bigger functions.
+// let sqrtOfNumbers = (num)=>{
+// return Math.sqrt(num)
+// }
+
+// let numbers = 16
+// let result = sqrtOfNumbers(numbers)
+// console.log(result)
