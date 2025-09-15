d4a07108cd65eac3c8042d9953f8a0db61adecfe
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-09-15 10:01:58 +0530

    Variables

---
+++ Variable/index.js
@@ -0,0 +1,38 @@
+// Var declaration............
+
+// var StudentName = "Shivam"
+// console.log(StudentName)
+
+// var StudentName = "Prakash"
+// console.log(StudentName)
+
+
+// let declaration............
+
+// let StudentName = "Shivam";
+// console.log(StudentName)
+
+// let StudentName 
+// StudentName = "Prakash"
+// console.log(StudentName)
+
+// Const declaration...............
+// const StudentName = "Prakash"
+// console.log(StudentName)
+
+// const StudentName
+// StudentName = "Prakash"
+// console.log(StudentName)
+
+// let StudentName = () =>{
+//   let a1 = "Mohan"
+//   console.log(a1)
+// }
+// StudentName()
+
+// let StudentName1 = () =>{
+//   let a1 = "Prakash"
+//   console.log(a1)
+// }
+// StudentName1()
+
