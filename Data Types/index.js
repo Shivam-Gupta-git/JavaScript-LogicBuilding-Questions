3292ea58c12285a9490d2955e9b76576e5bdda35
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-09-18 10:35:38 +0530

    Data type

---
+++ Data Types/index.js
@@ -0,0 +1,45 @@
+// Primitive data type.................
+// Number data type........
+// let digit1 = 1
+// console.log(typeof(digit1))
+
+// let digit2 = 4.6
+// console.log(typeof(digit2)) 
+
+// String data type.........
+// let studentName = "Mohan"
+// console.log(typeof(studentName))
+
+// let char = 'a'
+// console.log(typeof(char))
+
+// Boolean data type...........
+// let studentName = true
+// console.log(typeof(studentName))
+
+// Null data type.........
+// let data = null
+// console.log(typeof(data))
+
+// Undefine data type.......
+// let data;
+// console.log(typeof(data))
+
+// nom primitive data type.................
+// Object data type....
+
+// const student = {
+//   name: 'Prakash',
+//   age: 23,
+// }
+// console.log(student)
+
+// Array data type......
+// const fruite = ['apple', 'mango', 'orange']
+// console.log(fruite)
+
+// Functio data type
+// const sum = (num1, num2) => {
+//   return num1 + num2
+// }
+// console.log(sum(2, 5))
