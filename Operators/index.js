9ceada261bd83a26d2ed9b0d7b9593a3fb860147
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-09-18 10:36:16 +0530

    Operators

---
+++ Operators/index.js
@@ -0,0 +1,37 @@
+// -> Operator = Operator is a type of Symbol . that can be used to perform specific task.
+
+// -> Type of operators..........
+
+// 1. Arithmetic Operators = It is Used for mathematical operation.
+//  •	+ → Addition
+// let num1 = 5;
+// let num2 = 7
+// let sum = num1 + num2
+// console.log(sum)
+
+//	•	- → Subtraction
+// let num1 = 7;
+// let num2 = 5;
+// let sum = num1 - num2;
+// console.log(sum)
+
+//	•	* → Multiplication
+// let num1 = 7;
+// let num2 = 5;
+// let sum = num1 * num2;
+// console.log(sum)
+
+//	•	/ → Division (quotient)
+// let num1 = 7;
+// let num2 = 5;
+// let sum = num1 / num2;
+// console.log(sum)
+
+//	•	% → Modulus (remainder)
+// let num1 = 7;
+// let num2 = 5;
+// let sum = num1 % num2;
+// console.log(sum)
+
+
+
