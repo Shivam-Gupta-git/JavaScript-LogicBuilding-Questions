a88396b8df88922edca29b10a5afe634b918109a
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-09-20 17:19:17 +0530

    Destructuring assignment

---
+++ Destructuring assignment/ArrayDestructuring.js
@@ -0,0 +1,13 @@
+let number = [10, 20, 30, 40, 50]
+// -> Destructuring
+// let [a, b, c] = number
+// console.log(a, b, c)
+
+// -> skip values
+// let [a, ,b] = number
+// console.log(a, b)
+
+// -> add previous values
+// let [a, b, c, ...rest] = number
+// console.log(a, b, c, rest)
+
---
+++ Destructuring assignment/ObjectDestructuring.js
@@ -0,0 +1,17 @@
+let studentDetails = {
+  namee: "Mohan Kumar", 
+  age: 22,
+  cource: "B-tech",
+  branch: "CSE"
+}
+
+// let {namee, age} = studentDetails
+// console.log(namee, age)
+
+// -> Renaming variables:
+// const { namee: fullName, age: years } = studentDetails;
+// console.log(fullName, years);
+
+// -> Default values
+// const { country = "India" } = studentDetails;
+// console.log(country);
