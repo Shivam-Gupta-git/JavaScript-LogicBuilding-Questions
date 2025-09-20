46cccd35bb9505b2aeb0a0a861442f972583033f
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-09-20 17:19:51 +0530

    Spread Operator

---
+++ Spread Operator/index.js
@@ -0,0 +1,43 @@
+// -> Coppy array................
+// let arr1 = [1, 2, 3, 4, 5]
+// let arr2 = [...arr1]
+// console.log(arr2)
+
+// -> Merging two array............
+// let arr1 = [1, 2]
+// let arr2 = [2, 1]
+// let merge = [...arr1, ...arr2]
+// console.log(merge)
+
+// -> adding elements.................
+// let a = [12, 14]
+// let arr1 = [2, 4, 6, 8, 10]
+// let adding = [...arr1, ...a]
+// console.log(adding)
+
+// -> Copying objects..........
+// let studentDetails = {
+//   name: "Mohan",
+//   age: 22,
+//   city: "delhi"
+// }
+
+// let copyObject = {...studentDetails}
+// console.log(copyObject)
+
+// -> merge two object
+// let studentDetails = {
+//   name: "Mohan Sharma",
+//   age: 22,
+//   city: "Delhi"
+// }
+
+// let studentParentsDetails = {
+//   fatherName: "Santosh Sharma",
+//   contactNumber: 2345333111
+// }
+
+// let completeStudentDetails = {
+//   ...studentDetails, ...studentParentsDetails
+// } 
+// console.log(completeStudentDetails)
