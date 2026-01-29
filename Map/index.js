ac2b2dde2c37a51fa2d9ca30c96e0d29b633d28f
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-01-29 12:01:05 +0530

    Please provide the git diff so I can suggest an appropriate commit message.

---
+++ Map/index.js
@@ -0,0 +1,19 @@
+let myArray = [["a1", "Rohan Kumar"], ["a2", "Saurav Singh"]]
+
+let myMap = new Map(myArray);
+console.log(myMap);
+
+myMap.set("a3", "Ankit Sharma"); // set method is used to add the value in map. 
+console.log(myMap);
+
+myMap.set("a3", "Rahul Sharma");
+console.log(myMap);
+
+myMap.delete("a3");
+console.log(myMap); // delete method is used to delete the value from map.
+
+console.log(myMap.get("a1")); // get method is used to get the value from map.
+
+myMap.forEach((val , key) => {  // forEach method is used to itrate the map.
+  console.log(key, val)
+})
